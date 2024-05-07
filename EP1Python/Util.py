57fba6d1f5cc175192f2e0e8d81198ce1f15f7b4
EAlbiero <enzo.amattos@gmail.com>
2024-05-06 23:22:49 -0300

    Remove unecessary function from util

--- EP1Python/Util.py
+++ EP1Python/Util.py
@@ -5,23 +5,6 @@ import random as rd
 
 class Util():
 
-    def gera_entrada(m: int):
-        """
-        Recebe um int m como entrada e devolve uma string correspondente a
-        concatenação de cada dígito na base 2, sendo utilizados 4 bits por 
-        dígito.
-
-        Exemplo: gera_entrada(15) ---> '00010101'
-        """
-
-        m = str(m)
-        msg_final = format(int(m[0]), '04b')
-        for i in m[1:]:
-            novos_bits = format(int(i), '04b')
-            msg_final  += novos_bits
-
-        return msg_final
-
     def calcula_primo(t: int):
         """
         Calcula e devolve o menor número primo maior ou igual do que t utilizando
@@ -34,7 +17,7 @@ class Util():
             p = t
 
         while True:
-            if Util.miller_rabin(p, 40):
+            if Util.miller_rabin(p, 10):
                 break
             p += 2
 
